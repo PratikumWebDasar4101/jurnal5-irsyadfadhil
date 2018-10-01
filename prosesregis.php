a0283dc8f47e38ad813700ebb000ca9caf5ddcd4
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-01 10:36:00 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,16 @@
 <?php
-
-
-
-
-
-?>
+session_start();
+if (isset($_POST['submit'])) {
+ 	$nim = isset($_POST['nim']) ? $_POST['nim'] : "";
+ 	$nama = isset($_POST['nama']) ? $_POST['nama'] : "";
+ 	$email = isset($_POST['email']) ? $_POST['email'] : "";
+ }
+ echo "<center><h1>HASIL FORM REGISTRASI</h1></center>";
+ echo "NIM : ".$nim;
+ echo "<br>";
+ echo "<br>";
+ echo "Nama : ".$nama;
+ echo "<br>";
+ echo "<br>";
+ echo "Email : ".$email;
+ ?>
