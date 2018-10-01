57133bf4969acf77483aadc2a498549d13b94e7c
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-01 10:36:44 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Registrasi</title>
+ </head>
+ <body>
+ 	<center><h1>FORM REGISTRASI</h1></center>
+ 	<form action="prosesregis.php" method="POST">
+ 		NIM : <input type="text" name="nim" pattern="\d*" minlength="10" maxlength="10" title="Inputkan angka 10 digit" required><br><br>
+ 		Nama : <input type="text" name="nama" minlength="3" maxlength="25" title="Inputkan karakter minimal 3 maksimal 10" required><br><br>
+ 		Email : <input type="email" name="email" pattern="[a-z0-9._-]+@+[a-z]+.com" title="Inputkan email anda"required><br><br>
+ 		<input type="submit" name="submit">
+ 	</form>
+ </body>
+ </html>
