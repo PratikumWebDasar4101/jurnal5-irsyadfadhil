a100c749fc7a87253dbc14b8f6de5625f00076a7
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-01 10:35:17 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,27 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Komentar</title>
+</head>
+<body>
+	<center><h1>KOMENTAR</h1></center>
+	<form method="POST">
+		Nama : <input type="text" name="nama" maxlength="25" required=""><br><br>
+		<textarea name="komentar" cols="30" rows="10"></textarea><br><br>
+		<input type="submit" name="submit">
+	</form>
+	<?php 
+	if (isset($_POST['submit'])) {
+		$nama = $_POST['nama'];
+		$komentar = $_POST['komentar'];
 
-
-
-
-?>
+		$count = explode(" ", $komentar);
+		if (count($count) < 5) {
+			echo "Jumlah kata di komentar tidak sampai 5 kata";
+		} else {
+			echo "jumlah kata di komentar ada : ". count($count)." kata";
+		}
+	}
+ 	?>
+</body>
+</html>
